e234356b7cfeeafec886bdf1127274135217213b
Miroslav Vitkov <sir.vorac@gmail.com>
2018-02-27 15:25:49 +0100

    Yohoo! We now have a fully functional network manager in 20 LOC.

--- net/wifi-up.sh
+++ net/wifi-up.sh
@@ -2,11 +2,43 @@
 # Connects to a wifi network.
 # Do make the config file readable only by root!
 
+
 IFACE=wlp1s0
 CONF=/etc/wpa_supplicant/conf
 
-killall wpa_supplicant dhclient
-sleep 1
-wpa_supplicant -c"$CONF" -i$IFACE -B
-dhclient -v $(IFACE)
-ping abv.bg
+
+clean_up()
+{
+    killall wpa_supplicant dhclient
+    sleep 1
+}
+
+
+connect()
+{
+    wpa_supplicant -c"$CONF" -i"$IFACE" -B
+    dhclient -v "$IFACE"
+}
+
+
+# Continuously check that all is good.
+test()
+{
+    traceroute abv.bg
+}
+
+
+main()
+{
+    while [ true ]; do
+        clean_up
+        connect
+        until [ $? -ne 0 ]; do
+            test
+        done
+        echo; echo; echo; echo 'Connection lost! Reconnecting...'; echo
+    done
+}
+
+
+main
