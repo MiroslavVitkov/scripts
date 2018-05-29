ca9d9ec230abf30a294005cd5b2b0f9b8a0b497d
Miroslav Vitkov <sir.vorac@gmail.com>
2018-05-29 04:34:18 +0200

    [net] Added logging and espeak to connect.sh.

--- net/connect.sh
+++ net/connect.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Connects to a wifi network.
 # Do make the config file readable only by root!
 
@@ -7,8 +7,16 @@ IFACE=wlp1s0
 CONF=/etc/wpa_supplicant/conf
 
 
+# Used for debug tracing.
+log()
+{
+    echo "log: $1"
+}
+
+
 clean_up()
 {
+    log 'Cleaning up.'
     killall wpa_supplicant dhclient
     sleep 1
 }
@@ -16,6 +24,7 @@ clean_up()
 
 connect()
 {
+    log 'Connecting.'
     wpa_supplicant -c"$CONF" -i"$IFACE" -B
     dhclient -v "$IFACE"
 }
@@ -24,19 +33,56 @@ connect()
 # Continuously check that all is good.
 test()
 {
-    traceroute abv.bg
+    traceroute abv.bg 1> /dev/null 2>/dev/null
+    if [ $? -eq 0 ]; then
+        echo 'true'
+    else
+        echo 'false'
+    fi
 }
 
-# TODO: first test the connection!!!
+
+connection_lost()
+{
+    log 'Connection lost.'
+    espeak 'Internet connection lost! Reconnecting...'
+}
+
+
+connection_established()
+{
+    log 'Connection established.'
+    espeak 'Internet connection established.'
+}
+
+
 main()
 {
+    WAS_CONNECTED=$(test)
     while [ true ]; do
-        clean_up
-        connect
-        until [ $? -ne 0 ]; do
-            test
-        done
-        echo; echo; echo; echo 'Connection lost! Reconnecting...'; echo
+        CONNECTED=$(test)
+        log "state: $WAS_CONNECTED $CONNECTED"
+
+        if [[ "$WAS_CONNECTED" == 'false' && "$CONNECTED" == 'true' ]]; then
+            connection_established
+        fi
+
+        if [[ "$WAS_CONNECTED" == 'true' && "$CONNECTED" == 'false' ]]; then
+            connection_lost
+            clean_up
+            connect
+        fi
+
+        if [[ "$WAS_CONNECTED" == 'false' && "$CONNECTED" == 'false' ]]; then
+            clean_up
+            connect
+        fi
+
+        #if [[ "$WAS_CONNECTED" == 'true' && "$CONNECTED" == 'true' ]]; then
+            # All is fine.
+        #fi
+
+        WAS_CONNECTED="$CONNECTED"
     done
 }
 
