014b8a39ca792d04bd0e64bc25f6a33c92331eef
Miroslav Vitkov <sir.vorac@gmail.com>
2020-07-09 19:49:50 +0300

    [db] Refactoring.
    - indentation
    - use [[ instead of [
    - waste fewer lines for if clauses
    - introdice an error handling function
    - put constants on top
    
    All thanks to
    https://codereview.stackexchange.com/a/245233/66848

--- db/init.sh
+++ db/init.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 
 # This script initializes a new MariaDB database,
 # without root privileges,
 # and connects to it.
 # Learn SQL!
 # Pre-requisetes: `pacman -Syu mariadb`
+
+
+# Unprivilliged install.
+# For production this should be in `/opt`,
+# but this here is just for learning so let's be safe.
+DB_DIR="/home/$(id -u -n)/.sql/dat"
+SOCKET_DIR="/run/user/$(id -u)/mariadbtest"
+SOCKET="$SOCKET_DIR/mysql.socket"
 
 
 # Utility function.
@@ -18,27 +28,25 @@ function run
 }
 
 
-# Unprivilliged install.
-# For production this should be in `/opt`,
-# but this here is just for learning so let's be safe.
-DB_DIR="/home/$(id -u -n)/.sql/dat"
-SOCKET_DIR="/run/user/$(id -u)/mariadbtest"
-SOCKET="$SOCKET_DIR/mysql.socket"
+# Not used but too nice to pass on.
+# Use as `err "Unable to do_something"`.
+function err
+{
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
 
 
 # Create a DB if running for the first time.
-if [ ! -d "$DB_DIR" ]
-then
-mysql_install_db --datadir="$DB_DIR"
+if [[ ! -d "$DB_DIR" ]]; then
+    mysql_install_db --datadir="$DB_DIR"
 fi
 
 
 # Run the server if not running already.
 mkdir -p "$SOCKET_DIR"
 pidof mysqld
-if [ ! "$?" ]
-then
-run mysqld --socket="$SOCKET" --datadir="$DB_DIR"
+if [[ ! "$?" ]]; then
+    run mysqld --socket="$SOCKET" --datadir="$DB_DIR"
 fi
 
 
