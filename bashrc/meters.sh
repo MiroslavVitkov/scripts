b78656f9cd3408545e1f5a7998572f1551430362
Miroslav Vitkov <sir.vorac@gmail.com>
2023-01-10 08:41:00 +0200

    [bashrc] Remove the redundant 'Batery 0' message.

--- bashrc/meters.sh
+++ bashrc/meters.sh
@@ -4,7 +4,7 @@ while true
 do
     BR=$(cat /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness)
     BR_FRACTION=$(calc -d "$BR / 96000")
-    BAT=$(acpi | sed 's/Discharging/DISCHARGING/')
+    BAT=$(acpi | sed 's/Battery 0: //' | sed 's/Discharging/DISCHARGING/')
     TIME=$(date)
     CPU=$(uptime | cut -d' ' -f11,12,13)
     MEM=$(awk '/^Mem/ {print $4}' <(free -g))
