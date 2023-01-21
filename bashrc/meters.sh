89b486079003d50856b3558a3f5f8562673cbcd8
Miroslav Vitkov <sir.vorac@gmail.com>
2023-01-21 13:17:55 +0200

    [bashrc] Reworked meters to blink when battery is under 20%.
    The added calculations made the redraw flicker visible.
    Thus double buffering was introduced.

--- bashrc/meters.sh
+++ bashrc/meters.sh
@@ -1,20 +1,67 @@
 #!/usr/bin/bash
 
+
+# In this file: terminal based status bar. Intended for ratpoison.
+# TODO: perhaps include 'sensors' output in some way, for example the highest value?
+
+
+# Settings.
+BAT_CRITICAL=20  # Threshold for battery low, [%].
+REFRESH_PERIOD=2  # Seconds.
+BR_FILE='/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness'
+BR_MAX=96000  # Brightness, max allowable value.
+FIELD_SEPARATOR=';   '
+TMP_FILE="$(mktemp)"
+
+# Constants.
+RED=$(tput setaf 1)
+WHITE=$(tput setaf 7)
+DEFAULT=$(tput sgr0)
+
+
+# Read battery status and present it concisely.
+# If the battery is failing, blink to attract attention.
+function battery
+{
+    # Read the value, [%].
+    local BAT=$(acpi | sed 's/Battery 0: //' | sed 's/Discharging/DISCHARGING/')
+
+    # Blink bright colours if low and discharging.
+    local DISCHARGING=$(! echo "$BAT" | grep DISCHARGING > /dev/null ; echo "$?")
+    if [[ "$BAT" =~ ^\w+,\s+(\d+)% ]]; then local PERCENT="${BASH_REMATCH[1]}"; fi
+    if [[ "$DISCHARGING" -ne 0 ]] && [[ "$PERCENT" -le "$BAT_CRITICAL" ]]
+    then
+        if [[ "$BAT_COLOUR" == "$RED" ]]
+        then
+            BAT_COLOUR="$WHITE"
+        else
+            BAT_COLOUR="$RED"
+        fi
+    else
+        BAT_COLOUR="$DEFAULT"
+    fi
+
+    # Gloriously print our result respecting global separator setting.
+    printf "%s$FIELD_SEPARATOR" "${BAT_COLOUR}$BAT${DEFAULT}"
+}
+
+
 while true
 do
-    BR=$(cat /sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-eDP-1/intel_backlight/brightness)
-    BR_FRACTION=$(calc -d "$BR / 96000")
-    BAT=$(acpi | sed 's/Battery 0: //' | sed 's/Discharging/DISCHARGING/')
+    # Gather values.
+    BR=$(cat "$BR_FILE")
+    BR_FRACTION=$(calc -d "$BR / $BR_MAX")
     TIME=$(date)
     CPU=$(uptime | rev | cut -d' ' -f3,2,1 | rev)
     MEM=$(awk '/^Mem/ {print $4}' <(free -g))
 
-    clear
-    printf "%s;   " "$TIME"
-    printf "%s;   " "$BAT"
-    printf "brightness: %f;   " "$BR_FRACTION"
-    printf "%s;   " "$CPU"
-    printf "free: %sG" "$MEM"
-    sleep 2
+    printf "%s;   " "$TIME" > "$TMP_FILE"
+    battery >> "$TMP_FILE"
+    printf "brightness: %f;   " "$BR_FRACTION" >> "$TMP_FILE"
+    printf "%s;   " "$CPU" >> "$TMP_FILE"
+    printf "free: %sG" "$MEM" >> "$TMP_FILE"
 
+    clear
+    cat "$TMP_FILE"
+    sleep "$REFRESH_PERIOD"
 done
